2f07fc1483d81e5718e7ae5a659a5bc699cb4300
jovandosen <jovandosen1994@gmail.com>
2020-04-23 14:16:54 +0200

    Simple Provider example added

--- app/Providers/RiakServiceProvider.php
+++ app/Providers/RiakServiceProvider.php
@@ -3,21 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Services\Bar;
 
-class AppServiceProvider extends ServiceProvider
+class RiakServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
+     * Register services.
      *
      * @return void
      */
     public function register()
     {
-        //
+        $this->app->singleton(Bar::class, function ($app) {
+            return new Bar();
+        });
     }
 
     /**
-     * Bootstrap any application services.
+     * Bootstrap services.
      *
      * @return void
      */
