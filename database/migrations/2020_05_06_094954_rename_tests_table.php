2e5d6dfceafa8125f915400e5982175d56b3de01
jovandosen <jovandosen1994@gmail.com>
2020-05-06 13:00:48 +0200

    Migration test added

---
+++ database/migrations/2020_05_06_094954_rename_tests_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RenameTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if( Schema::hasTable('tests') ){
+            Schema::rename('tests', 'testing');
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if( Schema::hasTable('testing') ){
+            Schema::rename('testing', 'tests');
+        }
+    }
+}
