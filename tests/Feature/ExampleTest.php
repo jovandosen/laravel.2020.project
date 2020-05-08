6ea2de827fc05eb54316e91e4b546183250c2e67
jovandosen <jovandosen1994@gmail.com>
2020-05-08 17:26:28 +0200

    Few tests added

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,6 +16,48 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
+        // $response = $this->withCookie('color', 'blue')->get('/');
+
+        // $response->dumpHeaders();
+
+        // $response->dumpSession();
+
+        // $response->dump();
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test Foo
+     *
+     * @return void
+     */
+    public function testFooTest()
+    {
+        // $response = $this->get('/foo');
+
+        $response = $this->withSession(['aaa' => 'bbb'])->get('/foo');
+
+        // aaa is successfully set in session
+
+        // $response->dumpSession();
+
+        // $response->dump();
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test Bar
+     *
+     * @return void
+     */
+    public function testBarTest()
+    {
+        $response = $this->get('/bar');
+
+        // $response->dump();
+
         $response->assertStatus(200);
     }
 }
